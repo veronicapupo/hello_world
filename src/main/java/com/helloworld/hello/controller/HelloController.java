fcf33eab3af990512326510fb8eaf938f50eda33
veronica pupo <veronica.pupo@hotmail.com>
2022-08-29 09:57:05 -0300

    inserindo endpoint

--- src/main/java/com/helloworld/hello/controller/HelloController.java
+++ src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -4,12 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/helloworld")
     public String hello(){
         return "Hello Generation!!";
+    }
+
+    @GetMapping("/bsm")
+    public List<String> bsms(){
+
+        return List.of("Mentalidade de crescimento","Persistência","Responsabilidade pessoal","Orientação ao fututo","Comunicação","Orientação ao detalhe","Proatividade","Trabalho em equipe");
+    }
+
+    @GetMapping("/objetivos")
+    public String objetivos(){
+        return "Aprender Spring, Criar Projetos";
     }
 }
